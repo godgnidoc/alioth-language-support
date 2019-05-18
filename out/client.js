e4c36ea6566f585d3c22e85a071920abdafbf8dd
dn-ezr <stinger121@live.com>
2019-05-19 01:53:33 +0800

    get on the road

---
+++ out/client.js
@@ -0,0 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+const child_process_1 = require("child_process");
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "helloworld" is now active!');
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('alioth.initiateworkspace', () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        var folders = vscode.workspace.workspaceFolders;
+        if (folders) {
+            // vscode.window.showInformationMessage( folders[0].uri.path );
+            var cmd = "aliothc --init " + folders[0].uri.path;
+            child_process_1.exec(cmd);
+        }
+    });
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+// this method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+//# sourceMappingURL=client.js.map
